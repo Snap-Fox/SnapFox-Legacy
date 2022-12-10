1825719175ba465f04c9043e398747b2622877e3
Daniel Barton <mineminecraftpe687@gmail.com>
2022-12-10 12:52:12 +1100

    Commit

--- background.js
+++ background.js
@@ -1,21 +1,5 @@
-
-if (
-    chrome.runtime.getManifest().version_name.toLowerCase().includes("beta")
-  ) {
-    chrome.action.setBadgeText({ text: "BETA" });
-    chrome.action.setBadgeBackgroundColor({ color: "#ffae00" });
-  }
-
-  if (
-    chrome.runtime.getManifest().version_name.toLowerCase().includes("alpha")
-  ) {
-    chrome.action.setBadgeText({ text: "ALPHA" });
-    chrome.action.setBadgeBackgroundColor({ color: "#ffae00" });
-  }
 chrome.runtime.onInstalled.addListener(function (object) {
   if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
     chrome.tabs.create({ url: "https://snapfox.glitch.me" });
   }
 });
-
-
--- script.js
+++
@@ -1,3 +0,0 @@
-// 2020 SnapFox
-var myid = chrome.runtime.id;
-document.head.innerHTML += "<link rel='stylesheet' href='chrome-extension://"+ myid +"/style.css'>"
